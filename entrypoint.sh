c03a942de51a11f30f094ecceb1a534bd5ac4781
Dustin Rue <dustin.rue@10up.com>
2019-09-01 12:04:41 -0500

    make it better

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,13 @@
 
 # configure kubectl
 mkdir -p /root/.kube
+
 if [ "${K8S_SECRET_KUBERNETES_CONFIGURATION}" != "" ]; then
-  echo ${KUBERNETES_CONFIGURATION} | base64 -d > /root/.kube/config
-else
-  echo ${KUBERNETES_CONFIGURATION} | base64 -d > /root/.kube/config
+  KUBERNETES_CONFIGURATION=${K8S_SECRET_KUBERNETES_CONFIGURATION}
 fi
+
+
+echo ${KUBERNETES_CONFIGURATION} | base64 -d > /root/.kube/config
 
 exec "$@"
 
