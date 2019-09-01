ea6237c87094b21c91a0ed17dfeb911cf19cc042
Dustin Rue <dustin.rue@10up.com>
2019-09-01 12:01:12 -0500

    support gitlab

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,11 @@
 
 # configure kubectl
 mkdir -p /root/.kube
-echo ${KUBERNETES_CONFIGURATION} | base64 -d > /root/.kube/config
+if [ "${K8S_SECRET_KUBERNETES_CONFIGURATION}" != "" ]; then
+  echo ${KUBERNETES_CONFIGURATION} | base64 -d > /root/.kube/config
+else
+  echo ${KUBERNETES_CONFIGURATION} | base64 -d > /root/.kube/config
+fi
 
 exec "$@"
 
